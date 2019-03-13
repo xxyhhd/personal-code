4ee92ebf7022d355b79ec0f158056e9fb1d40c32
Rain XY19 Xu <xuxy19@Lenovo.com>
2019-03-13 16:39:54 +0800

    给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k

---
+++ mypython9.py
@@ -0,0 +1,19 @@
+## 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，
+## 使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+
+class Solution():
+    # 使用循环方法
+    def fun_one(self, nums, k):
+        for x in range(len(nums)):
+            if nums[x] in nums[x+k+1:len(nums)]:
+                return True
+        return False
+         
+
+nums = [2,6,4,5,7,2,6,4]
+k = 4   
+a = Solution()
+print(a.fun_one(nums, k))
+
+
+
